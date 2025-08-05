f999a8832c573d18ea8cf326700aacd3d3908116
susanavarela <sudava10@gmail.com>
2025-08-04 23:13:18 -0300

    initial commit

---
+++ application/plantix/src/main/java/com/laboratorio/iot/plantix/PlantixApplication.java
@@ -0,0 +1,13 @@
+package com.laboratorio.iot.plantix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantixApplication.class, args);
+	}
+
+}
---
+++ application/plantix/src/test/java/com/laboratorio/iot/plantix/PlantixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laboratorio.iot.plantix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
