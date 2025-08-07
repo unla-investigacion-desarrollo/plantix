bc56c17046f02df522abcb94943dc7e3d37c104c
Emiliano Ginarte Delgado <99384435+Emi3Dart@users.noreply.github.com>
2025-08-07 18:29:28 -0300

    estructura base de la arquitectura de las carpetas (#1)
    * estructura base
    
    * package dir fix
    
    link card de trello https://trello.com/c/JqEHLDhF/4-crear-proyecto-base-con-estructura

---
+++ application/plantix/src/main/java/com/laboratorio/iot/plantix/services/IMailService.java
@@ -0,0 +1,4 @@
+package com.laboratorio.iot.plantix.services;
+
+public interface IMailService {
+}
---
+++ application/plantix/src/main/java/com/laboratorio/iot/plantix/services/implementation/MailService.java
@@ -0,0 +1,8 @@
+package com.laboratorio.iot.plantix.services.implementation;
+
+import com.laboratorio.iot.plantix.services.IMailService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService implements IMailService {
+}
