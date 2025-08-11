83829c898c20461069982bb74411054627f2d6b1
EmilianoGinarte <eginartedelgado@gmail.com>
2025-08-11 18:16:17 -0300

    jpa incompleto

--- application/plantix/src/main/java/com/laboratorio/iot/plantix/entities/Device.java
+++ application/plantix/src/main/java/com/laboratorio/iot/plantix/entities/Device.java
@@ -1,19 +1,29 @@
 package com.laboratorio.iot.plantix.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+import jakarta.persistence.*;
 import java.util.List;
 
+@Entity
+@Table(name = "devices")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Device {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String type;
+
     private List<DeviceHistory> histories;
+
+
     private Field field;
 }
