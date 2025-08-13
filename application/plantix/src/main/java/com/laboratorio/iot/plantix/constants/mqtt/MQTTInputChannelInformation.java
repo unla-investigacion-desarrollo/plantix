84c1a5f7f065c2386fa09939fd256910011fe1f2
chocolatada21 <ulisesjustosaucedo@gmail.com>
2025-08-12 23:36:46 -0300

    Constants for MQTT Configuration

---
+++ application/plantix/src/main/java/com/laboratorio/iot/plantix/constants/mqtt/MQTTInputChannelInformation.java
@@ -0,0 +1,10 @@
+package com.laboratorio.iot.plantix.constants.mqtt;
+
+public class MQTTInputChannelInformation {
+    public static final String COMMON_CHANNEL = "commonInputChannel";
+    public static final String DHT11_TEMP_CHANNEL = "dht11TempInputChannel";
+    public static final String DHT11_HUMIDITY_CHANNEL = "dht11HumidityInputChannel";
+    public static final String SENSOR_SUBSTRATE_MOISTURE_CHANNEL = "sensorSubstrateMoistureInputChannel";
+    public static final String ELECTROVALVE_OPEN_CHANNEL = "electrovalveOpenInputChannel";
+    public static final String ELECTROVALVE_CLOSE_CHANNEL = "electrovalveCloseInputChannel";
+}
