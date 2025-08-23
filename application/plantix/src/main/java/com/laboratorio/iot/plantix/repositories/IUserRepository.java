b71e1f3faf021a0beca85794b0aa6254eefff890
chocolatada21 <ulisesjustosaucedo@gmail.com>
2025-08-23 16:37:01 -0300

    IUserRepository and UserDetailsServiceImpl for UserDetails

--- application/plantix/src/main/java/com/laboratorio/iot/plantix/repositories/IUserRepository.java
+++ application/plantix/src/main/java/com/laboratorio/iot/plantix/repositories/IUserRepository.java
@@ -1,4 +1,12 @@
 package com.laboratorio.iot.plantix.repositories;
 
-public interface IUserRepository {
+import com.laboratorio.iot.plantix.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
