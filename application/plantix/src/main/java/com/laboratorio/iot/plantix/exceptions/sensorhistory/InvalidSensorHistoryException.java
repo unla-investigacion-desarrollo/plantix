0525c027376ec2205354884d3b52a0419a9ff37f
chocolatada21 <ulisesjustosaucedo@gmail.com>
2025-09-21 19:31:58 -0300

    Validators and custom exceptions for Sensor and SensorHistory

---
+++ application/plantix/src/main/java/com/laboratorio/iot/plantix/exceptions/sensorhistory/InvalidSensorHistoryException.java
@@ -0,0 +1,7 @@
+package com.laboratorio.iot.plantix.exceptions.sensorhistory;
+
+public class InvalidSensorHistoryException extends RuntimeException {
+    public InvalidSensorHistoryException(String message) {
+        super(message);
+    }
+}
