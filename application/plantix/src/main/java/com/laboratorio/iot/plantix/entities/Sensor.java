2839d826499f8c41446b058365347a462a1ecf32
chocolatada21 <ulisesjustosaucedo@gmail.com>
2025-09-21 18:29:32 -0300

    Add @ToString annotation to improve logging

--- application/plantix/src/main/java/com/laboratorio/iot/plantix/entities/Sensor.java
+++ application/plantix/src/main/java/com/laboratorio/iot/plantix/entities/Sensor.java
@@ -1,10 +1,7 @@
 package com.laboratorio.iot.plantix.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 @Entity
@@ -13,6 +10,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Sensor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
