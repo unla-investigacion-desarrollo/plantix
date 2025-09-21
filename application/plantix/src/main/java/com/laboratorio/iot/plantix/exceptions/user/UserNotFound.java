d75743ecc124977d49a6afab276516ca9bcecabf
chocolatada21 <ulisesjustosaucedo@gmail.com>
2025-09-21 20:39:06 -0300

    Remove unused User exception

--- application/plantix/src/main/java/com/laboratorio/iot/plantix/exceptions/user/UserNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.laboratorio.iot.plantix.exceptions.user;
-
-public class UserNotFound extends RuntimeException {
-    public UserNotFound(String message) {
-        super(message);
-    }
-}
